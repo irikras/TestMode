b78f599f5c739fce28cdbb74ff61011ff97733b3
irikras <irikras@mail.ru>
2021-10-26 20:06:14 +0300

    Files and Tests updated

--- src/test/java/ru/netology/domain/DataGenerator.java
+++ src/test/java/ru/netology/domain/DataGenerator.java
@@ -1,10 +1,12 @@
 package ru.netology.domain;
 
 import com.github.javafaker.Faker;
+import com.sun.tools.xjc.reader.xmlschema.bindinfo.BIConversion;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.Value;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.util.Locale;
@@ -12,60 +14,58 @@ import java.util.Locale;
 import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
     private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given() // "дано"java -jar app-ibank.jar -P:profile=test
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    public static String getRandomLogin() {
+        String login = faker.name().fullName();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        String password = faker.internet().password();
+        return password;
     }
 
     public static class Registration {
         private Registration() {
         }
 
-        // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
-        private static RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://localhost")
-                .setPort(9999)
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
-
-        @BeforeAll
-        static void setUpAll(RegistrationData registrationData) {
-            given() // "дано"java -jar app-ibank.jar -P:profile=test
-                    .spec(requestSpec) // указываем, какую спецификацию используем
-                    .body(registrationData) // передаём в теле объект, который будет преобразован в JSON
-                    .when() // "когда"
-                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                    .then() // "тогда ожидаем"
-                    .statusCode(200); // код 200 OK
+        public static RegistrationDto getUser(String status) {
+            RegistrationDto user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            return user;
         }
 
-        public static RegistrationData getValidActiveUser() {
-            Faker faker = new Faker(new Locale("en"));
-            RegistrationData registrationData = new RegistrationData(
-                    faker.name().firstName(),
-                    faker.internet().password(),
-                    "active");
-            setUpAll(registrationData);
-            return registrationData;
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto registeredUser = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            sendRequest(registeredUser);
+            return registeredUser;
         }
+    }
 
-        public static RegistrationData getValidBlockedUser() {
-            Faker faker = new Faker(new Locale("en"));
-            RegistrationData registrationData = new RegistrationData(
-                    faker.name().firstName(),
-                    faker.internet().password(),
-                    "blocked");
-            setUpAll(registrationData);
-            return registrationData;
-        }
-
-        public static RegistrationData getUserWithoutRegistration() {
-            Faker faker = new Faker(new Locale("en"));
-            RegistrationData registrationData = new RegistrationData(
-                    faker.name().firstName(),
-                    faker.internet().password(),
-                    "active");
-            return registrationData;
-        }
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
     }
 }
--- src/test/java/ru/netology/domain/ServiceTest.java
+++ src/test/java/ru/netology/domain/ServiceTest.java
@@ -1,7 +1,9 @@
 package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
+import lombok.var;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -9,36 +11,34 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.domain.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.domain.DataGenerator.Registration.getUser;
+import static ru.netology.domain.DataGenerator.getRandomLogin;
+import static ru.netology.domain.DataGenerator.getRandomPassword;
 
 public class ServiceTest {
+
     @BeforeEach
-    void Setup() {
-       open("http://localhost:9999");
+    void setUp() {
+        open("http://localhost:9999");
     }
 
     @Test
-    void shouldSubmitActiveUser() {
-        RegistrationData validActiveUser = DataGenerator.Registration.getValidActiveUser();
-        $("[data-test-id=login] input").setValue(validActiveUser.getLogin());
-        $("[data-test-id=password] input").setValue(validActiveUser.getPassword());
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
         $(".button__text").click();
         $(withText("Личный кабинет")).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
     @Test
-    void shouldSubmitBlockedUser() {
-        RegistrationData validBlockedUser = DataGenerator.Registration.getValidBlockedUser();
-        $("[data-test-id=login] input").setValue(validBlockedUser.getLogin());
-        $("[data-test-id=password] input").setValue(validBlockedUser.getPassword());
-        $(".button__text").click();
-        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible,Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldSubmitIncorrectLogin() {
-        RegistrationData userWithoutRegistration = DataGenerator.Registration.getUserWithoutRegistration();
-        $("[data-test-id=login] input").setValue("Логин");
-        $("[data-test-id=password] input").setValue(userWithoutRegistration.getPassword());
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id=login] input").sendKeys(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(notRegisteredUser.getPassword());
         $(".button__text").click();
         $("[data-test-id=error-notification] .notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
@@ -46,10 +46,35 @@ public class ServiceTest {
     }
 
     @Test
-    void shouldSubmitIncorrectPassword() {
-        RegistrationData userWithoutRegistration = DataGenerator.Registration.getUserWithoutRegistration();
-        $("[data-test-id=login] input").setValue(userWithoutRegistration.getLogin());
-        $("[data-test-id=password] input").setValue("Пароль");
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id=login] input").sendKeys(blockedUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(blockedUser.getPassword());
+        $(".button__text").click();
+        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id=login] input").sendKeys(wrongLogin);
+        $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
+        $(".button__text").click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(wrongPassword);
         $(".button__text").click();
         $("[data-test-id=error-notification] .notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
